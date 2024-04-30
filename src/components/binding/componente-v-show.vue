072119585dcaa24862fdaadc6a07581eeddf6846
Billy <Billymartinezc@gmail.com>
2024-04-30 19:47:26 -0400

    Agrega nuevo componente con v-show
    Este commit incluye un nuevo componente Vue llamado "componente-v-show". Este nuevo componente utiliza la directiva v-show para controlar la visibilidad de diferentes elementos en la página.

---
+++ src/components/binding/componente-v-show.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "componente-v-show",
+  data() {
+    return {
+      mostrarMensaje: false
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Componente v-show</h1>
+    <p v-show="true">Este párrafo siempre se muestra</p>
+    <p v-show="false">Este párrafo nunca se muestra</p>
+    <button @click="mostrarMensaje = !mostrarMensaje">Mostrar/Ocultar mensaje</button>
+    <p v-show="mostrarMensaje">¡Hola, mundo!</p>
+  </div>
+</template>
