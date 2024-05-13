74eb43064b6c1c121609fa7216542c1d6f43e60b
Billy <Billymartinezc@gmail.com>
2024-05-13 19:25:13 -0400

    3ra parte tutorial

--- src/client/main.js
+++ src/client/main.js
@@ -1,8 +1,11 @@
 import { createApp} from 'vue'
 import {router} from "./router/router.js";
 import 'bootstrap/dist/css/bootstrap.css'
-
+import {createPinia} from "pinia";
 import App from './App.vue'
+
+const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
